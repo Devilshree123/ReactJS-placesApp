bcec9aa2a57795a326fd55a8a5de828f419fbba1
Shreyas <gopnarayanshreyas2004@gmail.com>
2022-11-08 11:36:35 +0530

    commit

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -7,7 +7,7 @@ const UsersList = (props) => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No Usern Found</h2>
+                <h2>No Users Found</h2>
             </div>
         );
     } else {
